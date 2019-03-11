23bfb8f0013e5606374a42c4b885a509905bdd34
coline <coline@126.com>
2019-03-11 10:09:04 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+// 1.引入axios
+import axios from 'axios';
 
+Vue.use(iView);
+// 2.把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
